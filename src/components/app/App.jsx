afd9193c3803d7537935ca314896cbe376a8e88f
Juju-And <justyna.andrejczuk@gmail.com>
2018-11-25 15:38:36 +0000

    First change

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import styles from "./App.scss";
+import imageSrc from "../../assets/cookie.jpeg";
 
 export default class App extends React.Component {
     render() {
-        return <div> To jest Appkaaaaaaaaaaaaaa</div>;
+        return (<div className={styles.app}> To jest Appkaaaaaaaaaaaaaa
+        <img src={imageSrc} alt="Me loves cookies!" />
+        </div>);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,40 @@ module.exports = {
         filename: "app.js",
         path: __dirname + "/docs"
     },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
     module: {
         rules: [
             {
-                test: /.jsx?/,
+                test: /\.jsx?/,
                 loader: "babel-loader",
                 exclude: /node_modules/,
                 options: {
                     presets: ["@babel/preset-react"]
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: "style-loader"}, // creates style nodes from JS strings
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]__[local]--[hash:base64:5]',
+                        }
+                    }, // translates CSS into CommonJS
+                    {loader: "sass-loader"} // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                        loader: "url-loader"
+                    }
+                ]
             }
         ]
     }
